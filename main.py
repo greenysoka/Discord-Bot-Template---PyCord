f0d8aea280454fb4296b248eb7c96e61244f7fa6
The-GreenCraft <th3greencraft@gmail.com>
2024-05-08 09:17:03 +0200

    comments & command name change

--- main.py
+++ main.py
@@ -15,26 +15,27 @@ intents.messages = True
 intents.presences = True 
 
 
-
+# configure your bot. You'll need it for all commands and ui components
+# If you only use slash commands, you don't need a command_prefix
 bot = commands.Bot(command_prefix='!bot', intents=intents)
 
-
+# Send a message in your console when the bot is ready.
 @bot.event
 async def on_ready():
     print(f">> Started as {bot.user.name} | Ping: {bot.latency}")
+    # Change the bot's status
     await bot.change_presence(activity=discord.CustomActivity("Template is online. Congrats!"), status=discord.Status.online)
 
 
-
-
-
-
+# This is an example for a slash command. You have to give permission for slash commands.
 @bot.slash_command(
         name="ping",
         description="Check the ping for the template bot!")
-async def record(ctx):
+async def pingcmd(ctx):
+    # Define what should happen on /ping command
     await ctx.respond(f"Bot is online! > Ping: {bot.latency}s")
-    
+
+# Get your TOken from the .env File and start bot. Don't change these lines
 token = str(os.getenv("TOKEN"))
 bot.run(token)
 
